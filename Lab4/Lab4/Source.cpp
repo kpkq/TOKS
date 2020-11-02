ae0191a04743a845127b66dae947580e0b0ad69f
kpkq <you@example.com>
2020-11-02 14:11:13 +0300

    lab4

---
+++ Lab4/Lab4/Source.cpp
@@ -0,0 +1,67 @@
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <thread>
+#define COLLISION_WINDOW 350
+#define JAM_SYMBOL '-'
+
+using namespace std;
+
+char chanel = ' ';
+
+void transmitter(string data) {
+
+	int attempts;
+	int k;
+	int shift = 0;
+
+	for(auto msg: data) {
+
+		attempts = 0;
+
+		while (1) {
+			while (chanel != ' ');	// ожидание освобождения канала
+			chanel = msg;	// передача кадра
+			Sleep(COLLISION_WINDOW);	// выжидание окна коллизии
+			if (chanel == msg)	// проверка коллизии
+			{
+				chanel = ' ';	// очищаем канал
+				break;	// выход
+			}
+			chanel = (char)JAM_SYMBOL;	// отправка Jam сигнала
+			attempts++;
+			if (attempts == 10) // выход при превышении числа попыток
+			{
+				cout << "ERROR" << endl;
+				return;
+			}
+			Sleep((rand() % ((int)pow(2, attempts) - 1)) * 25);    // случайная задержка
+		}
+
+		shift++;
+	}
+
+	chanel = ' ';
+}
+
+int main(int argc, char* argv[]) {
+	thread transmitter1(transmitter, "1234567890~");
+	thread transmitter2(transmitter, "!@#$%^&*()~");
+	thread transmitter3(transmitter, "qwertyuiop~");
+	while (1) {
+
+		Sleep(100);
+		cout << chanel;
+		if (chanel == (char)JAM_SYMBOL)
+		{
+			chanel = ' ';
+			continue;
+		}
+
+	}
+
+	transmitter1.join();
+	transmitter2.join();
+	transmitter3.join();
+	return 0;
+}
